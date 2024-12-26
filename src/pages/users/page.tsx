ffaebc16e2be27357ce218dec26b42697923d5a3
Art San <artsan.mcc@gmail.com>
2024-12-26 18:51:43 +0700

    end

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,20 +1,23 @@
-import { use, useState, Suspense } from 'react'
-import { User, createUser, fetchUsers } from '../../shared/api'
+import { use, useState, Suspense, useTransition, startTransition } from 'react'
+import { User, createUser, deleteUser, fetchUsers } from '../../shared/api'
 
 const defaultUsersPromises = fetchUsers()
 
 export function UsersPage() {
   const [usersPromise, setUsersPromises] = useState(defaultUsersPromises)
-  const refetchUser = () => {
-    setUsersPromises(fetchUsers)
-  }
+  const refetchUser = () => startTransition(() => setUsersPromises(fetchUsers))
+
+  // Первый вар
+  // const refetchUser = () => {
+  //   setUsersPromises(fetchUsers)
+  // }
 
   return (
     <main className=" container mx-auto p-4 pt-10 flex flex-col gap-4">
       <div className=" text-3xl font-bold underline mb-10">UserPage</div>
       <CreateUserForm refetchUser={refetchUser} />
       <Suspense fallback={'Loading...'}>
-        <UsersList usersPromise={usersPromise} />
+        <UsersList usersPromise={usersPromise} refetchUser={refetchUser} />
       </Suspense>
     </main>
   )
@@ -22,15 +25,24 @@ export function UsersPage() {
 
 export function CreateUserForm({ refetchUser }: { refetchUser: () => void }) {
   const [email, setEmail] = useState('')
+  const [isPending, startTransition] = useTransition()
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    await createUser({
-      email,
-      id: crypto.randomUUID()
+    startTransition(async () => {
+      await createUser({
+        email,
+        id: crypto.randomUUID()
+      })
+      refetchUser()
+      setEmail('')
+
+      // Первый вар
+      // startTransition(() => {
+      //   refetchUser()
+      //   setEmail('')
+      // })
     })
-    refetchUser()
-    setEmail('')
   }
   return (
     <form className=" flex gap-2" onSubmit={handleSubmit}>
@@ -38,36 +50,71 @@ export function CreateUserForm({ refetchUser }: { refetchUser: () => void }) {
         type="email"
         className=" border p-2 rounded"
         value={email}
+        disabled={isPending}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="введите email"
       />
 
-      <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        add
+      <button
+        disabled={isPending}
+        className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-400"
+      >
+        {'add'}
       </button>
     </form>
   )
 }
 
-export function UsersList({ usersPromise }: { usersPromise: Promise<User[]> }) {
+export function UsersList({
+  usersPromise,
+  refetchUser
+}: {
+  usersPromise: Promise<User[]>
+  refetchUser: () => void
+}) {
   const users = use(usersPromise)
   // console.log(123, users)
   return (
     <div className=" flex flex-col">
       {users.map((user) => (
-        <UserCard key={user.id} user={user} />
+        <UserCard key={user.id} user={user} refetchUser={refetchUser} />
       ))}
     </div>
   )
 }
 
-export function UserCard({ user }: { user: User }) {
+export function UserCard({
+  user,
+  refetchUser
+}: {
+  user: User
+  refetchUser: () => void
+}) {
+  const [isPending, startTransition] = useTransition()
+
+  const handleDel = async (id: string) => {
+    startTransition(async () => {
+      await deleteUser(id)
+      refetchUser()
+    })
+  }
+
+  // Первый вар
+  // const handleDel = async (id: string) => {
+  //   startTransition(async () => {
+  //     await deleteUser(id)
+  //     startTransition(() => refetchUser())
+  //   })
+  // }
+
   return (
     <div className=" border p-2 m-2 rounded bg-gray-100 flex gap-2">
       {user.email}
       <button
-        className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto"
         type="button"
+        onClick={() => handleDel(user.id)}
+        disabled={isPending}
+        className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto disabled:bg-gray-400"
       >
         delete
       </button>
