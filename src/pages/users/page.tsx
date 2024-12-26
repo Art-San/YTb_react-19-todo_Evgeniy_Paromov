69d2476c6b67916a2cb90d127e4430628c44f3bf
Art San <artsan.mcc@gmail.com>
2024-12-26 16:55:46 +0700

    end

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,63 +1,76 @@
-import { useState } from 'react'
+import { use, useState, Suspense } from 'react'
+import { User, createUser, fetchUsers } from '../../shared/api'
 
-type User = {
-  id: string
-  email: string
-}
+const defaultUsersPromises = fetchUsers()
 
 export function UsersPage() {
-  const [users, setUsers] = useState<User[]>([])
-  const [email, setEmail] = useState('')
-
-  const handleSubmit = (event: React.FocusEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    setUsers([...users, { id: crypto.randomUUID(), email: email }])
-    setEmail('')
-  }
-
-  const handleDel = (id: string) => {
-    setUsers((lastUsers) => lastUsers.filter((user) => user.id !== id))
+  const [usersPromise, setUsersPromises] = useState(defaultUsersPromises)
+  const refetchUser = () => {
+    setUsersPromises(fetchUsers)
   }
 
   return (
     <main className=" container mx-auto p-4 pt-10 flex flex-col gap-4">
       <div className=" text-3xl font-bold underline mb-10">UserPage</div>
-      <section className="">
-        <form className=" flex gap-2" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            className=" border p-2 rounded"
-            value={email}
-            placeholder="введите email"
-            onChange={(event) => setEmail(event.target.value)}
-          />
-
-          <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            add
-          </button>
-        </form>
-      </section>
-      <section>
-        <div className=" flex flex-col">
-          {users.map((user) => (
-            <div
-              className=" border p-2 m-2 rounded bg-gray-100 flex gap-2"
-              key={user.id}
-            >
-              {user.email}
-              <button
-                className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto"
-                type="button"
-                onClick={() => handleDel(user.id)}
-              >
-                delete
-              </button>
-            </div>
-          ))}
-        </div>
-      </section>
+      <CreateUserForm refetchUser={refetchUser} />
+      <Suspense fallback={'Loading...'}>
+        <UsersList usersPromise={usersPromise} />
+      </Suspense>
     </main>
   )
 }
 
-export function UsersList() {}
+export function CreateUserForm({ refetchUser }: { refetchUser: () => void }) {
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    await createUser({
+      email,
+      id: crypto.randomUUID()
+    })
+    refetchUser()
+    setEmail('')
+  }
+  return (
+    <form className=" flex gap-2" onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className=" border p-2 rounded"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="введите email"
+      />
+
+      <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        add
+      </button>
+    </form>
+  )
+}
+
+export function UsersList({ usersPromise }: { usersPromise: Promise<User[]> }) {
+  const users = use(usersPromise)
+  // console.log(123, users)
+  return (
+    <div className=" flex flex-col">
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  )
+}
+
+export function UserCard({ user }: { user: User }) {
+  return (
+    <div className=" border p-2 m-2 rounded bg-gray-100 flex gap-2">
+      {user.email}
+      <button
+        className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto"
+        type="button"
+      >
+        delete
+      </button>
+    </div>
+  )
+}
